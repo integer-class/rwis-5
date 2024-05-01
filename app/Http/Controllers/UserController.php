81cea3aabee2465bcf0e4c430cd26e2b652afbc0
Davis Maulana Hermanto <davismaulana03@gmail.com>
2024-05-01 13:52:36 +0700

    Upload wir

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $users = DB::table('users')
+        ->when($request->keyword, function ($query) use ($request) {
+            $query->where('name', 'like', "%{$request->keyword}%")
+                ->orWhere('email', 'like', "%{$request->keyword}%")
+                ->orWhere('phone', 'like', "%{$request->keyword}%");
+        })
+        ->paginate(5);
+        return view('pages.user.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($request->input('password'));
+        User::create($data);
+        return redirect()->route('user.index');
+    }
+
+    public function show($id)
+    {
+        return view('pages.dashboard');
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('pages.user.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $user = User::findOrFail($id);
+
+        if ($request->input('password')) {
+            $data['password'] = Hash::make($request->input('password'));
+        } else {
+            unset($data['password']);
+        }
+        $user->update($data);
+        return redirect()->route('user.index');
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('user.index');
+    }
+}
