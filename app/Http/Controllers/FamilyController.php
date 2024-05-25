9fb18c85bcde1a725d78b03bac2d79e729520a36
srikresna <srikresna383@gmail.com>
2024-05-25 22:48:09 +0700

    feat: family data - edit utility

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\FamilyModel; 
+
+use App\Models\FamilyModel;
 use App\Models\CitizenDataModel;
 use Illuminate\Http\Request;
 
 class FamilyController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         $Keyword = $request->keyword;
         $famMemberCount = CitizenDataModel::select('family_data.family_head_name', 'family_data.family_id')
             ->join('family_data', 'family_data.family_id', '=', 'citizen_data.family_id')
             ->where('family_data.is_archived', false)
             ->get()
             ->groupBy('family_id')
-            ->map(function($item) {
+            ->map(function ($item) {
                 return $item->count();
             });
         if ($Keyword) {
@@ -29,17 +31,24 @@ class FamilyController extends Controller
                 session()->flash('message', 'Pencarian ditemukan: ' . $Keyword);
                 session()->flash('alert-class', 'alert-success');
             }
-
         } else {
             $families = FamilyModel::select('family_data.*')
                 ->where('family_data.is_archived', false)
                 ->paginate(8);
-    
         }
         return view('pages.family.index', compact('families', 'famMemberCount'));
     }
 
-    public function detail($id) {
+    public function archive($id)
+    {
+        $family = FamilyModel::find($id);
+        $family->is_archived = true;
+        $family->save();
+        return redirect()->route('family.index');
+    }
+
+    public function detail($id)
+    {
         $family = FamilyModel::find($id);
         $famMembers = CitizenDataModel::select('citizen_data.*', 'wealth_data.education', 'wealth_data.job')
             ->join('wealth_data', 'wealth_data.wealth_id', '=', 'citizen_data.wealth_id')
@@ -49,14 +58,16 @@ class FamilyController extends Controller
         return view('pages.family.detail', compact('family', 'famMembers'));
     }
 
-    public function create() {
+    public function create()
+    {
         $citizens = CitizenDataModel::select('citizen_data.*')
             ->where('citizen_data.is_archived', false)
             ->paginate(5);
         return view('pages.family.create', compact('citizens'));
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $family = new FamilyModel();
         $family->family_id = $request->family_id;
         $family->family_head_name = $request->family_head_name;
@@ -76,5 +87,48 @@ class FamilyController extends Controller
             $citizen->save();
         }
         return redirect()->route('family.index');
+    }
+
+    public function edit($id)
+    {
+        $family = FamilyModel::find($id);
+        $famMembers = CitizenDataModel::select('citizen_data.*', 'wealth_data.education', 'wealth_data.job')
+            ->join('wealth_data', 'wealth_data.wealth_id', '=', 'citizen_data.wealth_id')
+            ->where('citizen_data.is_archived', false)->paginate(5);
+        return view('pages.family.edit', compact('family', 'famMembers'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $family = FamilyModel::find($id);
+        $family->family_id = $request->family_id;
+        $family->family_head_name = $request->head_name;
+        $family->address = $request->address;
+        $family->rt = $request->rt;
+        $family->rw = $request->rw;
+        $family->village = $request->village;
+        $family->sub_district = $request->sub_district;
+        $family->city = $request->city;
+        $family->province = $request->province;
+        $family->postal_code = $request->postal_code;
+        $family->save();
+
+        $familyId = $family->family_id;
+        // jika pengguna menambahkan anggota keluarga baru, tambahkan anggota keluarga tersebut ke dalam keluarga
+        // jika pengguna menghapus anggota keluarga, hapus anggota keluarga tersebut dari keluarga
+        $citizen= CitizenDataModel::select('citizen_data.*')
+            ->where('family_id', $familyId)
+            ->get();
+        foreach ($citizen as $c) {
+            $c->family_id = null;
+            $c->save();
+        }
+        foreach ($request->citizens as $citizenId) {
+            $citizen = CitizenDataModel::find($citizenId);
+            $citizen->family_id = $familyId;
+            $citizen->save();
+        }
+
+        return redirect()->route('family.detail', $id);
     }
 }
